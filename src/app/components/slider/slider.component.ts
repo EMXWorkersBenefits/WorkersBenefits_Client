fafea81d0b7cfb7326ffd0ad658a46f00c2786ca
TaliG <taligold.17@gmail.com>
2017-08-15 00:54:36 +0300

    1.Added a side description of the photos in the slider. 2. Added a button

--- src/app/components/slider/slider.component.ts
+++ src/app/components/slider/slider.component.ts
@@ -1,4 +1,5 @@
 import {AfterViewInit, Component, ContentChildren, OnInit, QueryList} from '@angular/core';
+import {Description} from '../../models/description.model';
 
 @Component({
   selector: 'app-slider',
@@ -7,6 +8,17 @@ import {AfterViewInit, Component, ContentChildren, OnInit, QueryList} from '@ang
 })
 export class SliderComponent implements OnInit, AfterViewInit {
   @ContentChildren('frame') children: QueryList<any[]>;
+  descriptions: Description[] = [
+    new Description('ארוחה זוגית במסעדת Branja התל אביבית במתחם שרונה',
+      'מסעדת ברנזה של המסעדן הראל בלו משיקה תפריט קיץ חדש ומציעה ארוחה זוגית בת 8 מנות',
+      39),
+    new Description('מנוי שנתי למגוון חדרי כושר ברחבי הארץ',
+      'מנוי שנתי לחדרי הכושר המובילים בארץ בהנחות של עד כ- 40%',
+      299),
+    new Description('ספטמבר במונטנגרו',
+      'חבילת נופש הכוללת טיסות הלוך וחזור ו-4 לילות במלון לבחירה ע"ב חצי פנסיון ',
+      2999),
+  ];
   index = 0;
   isPlaying = false;
   images: String[] = ['/assets/img/food.jpg', '/assets/img/gym.jpg', '/assets/img/montenegro.jpg'];
