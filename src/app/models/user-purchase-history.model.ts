d9b5e04ed50e9fb258b1d32899fad8260f6a45e6
TaliG <taligold.17@gmail.com>
2017-08-19 13:07:54 +0300

    1. Added a new model to describe userPurchaseHistory's required fields 2. Trying to add to Git files that were not included when created -models, user-purchase-history and picture-grid

---
+++ src/app/models/user-purchase-history.model.ts
@@ -0,0 +1,21 @@
+export class UserPurchaseHistory {
+  public deal: string;
+  public img: string;
+  public purchased: boolean;
+  public price: number;
+  public discount: number;
+  public expires: string;
+  constructor (
+    deal: string,
+    img: string,
+    purchased: boolean,
+    price: number,
+    discount: number,
+    expires: string) {
+    this.deal = deal;
+    this.img = img;
+    this.purchased = purchased;
+    this.discount = discount;
+    this.expires = expires;
+  }
+}
---
+++ src/app/picture-grid/picture-grid.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-picture-grid',
+  templateUrl: './picture-grid.component.html',
+  styleUrls: ['./picture-grid.component.css']
+})
+export class PictureGridComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
